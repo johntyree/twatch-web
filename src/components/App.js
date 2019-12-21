656d31a037da5e21ef466501a8c6ed963436f8f3
John Tyree <johntyree@gmail.com>
2019-12-21 14:14:19 -0800

    Routing set up

--- src/components/App.js
+++ src/components/App.js
@@ -1,25 +1,28 @@
 import React from 'react';
-import { BrowserRouter, Route} from 'react-router-dom';
+import { HashRouter, Route} from 'react-router-dom';
+import Header from './Header';
+import StreamCreate from './streams/StreamCreate';
+import StreamEdit from './streams/StreamEdit';
+import StreamDelete from './streams/StreamDelete';
+import StreamList from './streams/StreamList';
+import StreamShow from './streams/StreamShow';
 
-const PageOne = () => {
-    return <div>Page One</div>;
-}
 
-
-const PageTwo = () => {
-    return <div>Page Two</div>;
-}
 const App = () => {
-    return (
+  return (
+    <div className="ui container">
+      <HashRouter>
+        <Header />
         <div>
-            <BrowserRouter>
-            <div>
-              <Route path="/" exact component={PageOne} />
-              <Route path="/pagetwo" exact component={PageTwo} />
-            </div>
-            </BrowserRouter>
+          <Route path="/" exact component={StreamList} />
+          <Route path="/streams/new" exact component={StreamCreate} />
+          <Route path="/streams/edit" exact component={StreamEdit} />
+          <Route path="/streams/delete" exact component={StreamDelete} />
+          <Route path="/streams/show" exact component={StreamShow} />
         </div>
-    );
+      </HashRouter>
+    </div>
+  );
 }
 
 
