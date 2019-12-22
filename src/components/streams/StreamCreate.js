7ec564ab5dd215149f13130bf0c5e184e319f60d
John Tyree <johntyree@gmail.com>
2019-12-22 20:29:30 +0000

    Add a pretty form to StreamCreate

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -1,9 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Field, reduxForm } from 'redux-form';
 
-const StreamCreate = () => {
-  return (
-    <div>StreamCreate</div>
-  );
-};
 
-export default StreamCreate;
+class StreamCreate extends Component {
+
+  renderInput({ input, label }) {
+    return (
+      <div className="field">
+        <label>{ label }: </label>
+        <input {...input} />
+      </div>
+    );
+  }
+
+  render() {
+    return (
+      <form className="ui form">
+        <Field name='title' component={this.renderInput} label="Enter Title" />
+        <Field name='description' component={this.renderInput} label="Enter Description" />
+      </form>
+    );
+  }
+}
+
+export default reduxForm({
+  form: 'streamCreate',
+})(StreamCreate);
