ed6dcac8e1c0d697a0f1fae6768c5496e455d579
John Tyree <johntyree@gmail.com>
2019-12-24 17:35:18 +0000

    Use custom router history.

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,18 @@
 import React from 'react';
-import { HashRouter, Route} from 'react-router-dom';
+import { Router, Route} from 'react-router-dom';
 import Header from './Header';
 import StreamCreate from './streams/StreamCreate';
 import StreamEdit from './streams/StreamEdit';
 import StreamDelete from './streams/StreamDelete';
 import StreamList from './streams/StreamList';
 import StreamShow from './streams/StreamShow';
+import history from '../history';
 
 
 const App = () => {
   return (
     <div className="ui container">
-      <HashRouter>
+      <Router history={history}>
         <Header />
         <div>
           <Route path="/" exact component={StreamList} />
@@ -20,7 +21,7 @@ const App = () => {
           <Route path="/streams/delete" exact component={StreamDelete} />
           <Route path="/streams/show" exact component={StreamShow} />
         </div>
-      </HashRouter>
+      </Router>
     </div>
   );
 }
