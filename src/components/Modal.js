6325331ce163274153bcb45872d8a7a613d42f07
John Tyree <johntyree@gmail.com>
2020-02-02 05:14:58 +0000

    Pass configuration to Modal from parent component.

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -4,9 +4,15 @@ import ReactDOM from 'react-dom';
 
 const Modal = props => {
   return ReactDOM.createPortal(
-    <div className = 'ui dimmer modals visible active'>
-      <div className="ui standard modal visible active">
-        balls
+    <div
+        onClick={props.onDismiss}
+        className='ui dimmer modals visible active'>
+      <div
+          onClick={(e) => e.stopPropagation()} 
+          className="ui standard modal visible active">
+        <div className="header">{props.title}</div>
+        <div className="content">{props.content}</div>
+        <div className="actions">{props.actions}</div>
       </div>
     </div>,
     document.querySelector('#modal')
--- src/components/streams/StreamDelete.js
+++ src/components/streams/StreamDelete.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import Modal from '../Modal';
+import history from '../../history';
 
 
 const StreamDelete = () => {
+  const actions = (
+    <React.Fragment>
+      <button className="ui button negative">Delete</button>
+      <button className="ui button">Cancel</button>
+    </React.Fragment>
+  );
   return (
     <div>
       StreamDelete
-      <Modal />
+      <Modal
+        title="Delete Stream"
+        content="Are you sure you want to cross the stream?"
+        actions={actions}
+        onDismiss={() => history.push('/')}
+      />
     </div>
   );
 };
